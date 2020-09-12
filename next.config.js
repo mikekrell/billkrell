2d010e0171783d77c78a0a05086e63892c4d108e
mikekrell <michaelkrell@gmail.com>
2020-09-12 01:36:59 -0700

    remove dotenv

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const next_config = {
         config.plugins = [
             ...config.plugins,
             // Read the .env file
-            new Dotenv({
-                path: path.join(__dirname, '.env'),
-                systemvars: true
-            })
         ];
 
         return config;
@@ -28,5 +24,15 @@ module.exports = {
                 permanent: true,
             },
         ]
+    },
+    env : {
+        CONTENTFUL_SPACE:"ayaqw4ri6qlp",
+        CONTENTFUL_TOKEN:"LeSE5FXyc3zbLi-tPJbcw_dn88qLIdqUlUYv-Q2FClw",
+        MAILCHIMP_API_KEY:"cf5488b5ada2f48735e0bcc2169d2204-us17",
+        MAILCHIMP_SERVER_PREFIX : "us17",
+        MAILCHIMP_LIST_ID : "eade3795f0",
+        TWILIO_ACCOUNT_SID: "AC03623229dba462b92876e9bec4f98181",
+        TWILIO_AUTH_KEY : "005bd078cc1cc22a5eb099af3916ed3b",
+        CLOZE_API_KEY : "0c1a695d08bb40831679c0e87ef9856b",
     }
 };
