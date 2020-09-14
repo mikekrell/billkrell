80a211588fd6af50b06103ba95ea8460811fd324
mikekrell <michaelkrell@gmail.com>
2020-09-13 18:17:05 -0700

    better padding

--- components/EquipmentCard.js
+++ components/EquipmentCard.js
@@ -17,18 +17,21 @@ function EquipmentCard( { equip }){
                         {equip.fields.media !== undefined ? <img src={getImageUrl(equip.fields.media[0])} className="image" width="200" height="auto" alt={equip.fields.title}></img> : <img src="nopreview.jpg" className="image" width="200" height="auto" alt={equip.fields.title}></img>}
                         </figure>
                         <div class="card-content is-overlay is-clipped">
-                        <span className={equip.fields.price == 0 ? 'tag is-pulled-right has-background-warning is-medium' : 'tag is-pulled-right has-background-success has-text-white-ter is-medium' }>
-                            {equip.fields.price == 0 ? `Call for Quote` : formatter.format(equip.fields.price) }
-                        </span>
-                        <span class="tags has-addons" style={{ "opacity": "0.5"}}>
-                            <span class="tag" style={{ "opacity": "1" }}>{equip.fields.media !== undefined  && equip.fields.media.length > 0 ? equip.fields.media.length : '0'}</span>
-                            <span class="tag" style={{ "opacity": "1" }}><FontAwesomeIcon className="icon is-small" icon={faImage} /></span>
-                        </span>
+                        <span className={equip.fields.price == 0 ? 'tag is-pulled-right has-background-warning is-medium' : 'tag is-pulled-right is-medium has-background-success has-text-white-ter' }>
+                                {equip.fields.price == 0 ? `Call for Quote` : formatter.format(equip.fields.price) }
+                            </span>
+                            <span class="tags has-addons" style={{ "opacity": "0.5"}}>
+                                <span class="tag" style={{ "opacity": "1" }}>{equip.fields.media !== undefined  && equip.fields.media.length > 0 ? equip.fields.media.length : '0'}</span>
+                                <span class="tag" style={{ "opacity": "1" }}><FontAwesomeIcon className="icon is-small" icon={faImage} /></span>
+                            </span>
                         </div>
                     </div>
                     <span className="image-title">
-                    <p className="is-size-6 has-text-white image-manufacture-title"><span className="has-text-weight-bold">{equip.fields.year}</span> {equip.fields.manufacture}</p>
-                        <h1 className="is-size-4 has-text-white has-text-weight-bold">{equip.fields.title}</h1>
+                    <p className="is-size-5 has-text-white image-manufacture-title"><span className="has-text-weight-bold">{equip.fields.year}</span> {equip.fields.manufacture}</p>
+                        <h1 className="is-size-2 has-text-white has-text-weight-bold">{equip.fields.title}</h1>
+                    </span>
+                    <span className="more-info">
+                        <p></p>
                     </span>
                 </div>
             </div>
